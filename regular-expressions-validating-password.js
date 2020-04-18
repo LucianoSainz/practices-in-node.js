3745833506b63d1ea6aa242ca2ead88518ce9bc3
luciaoSainz <danielsainz28@gmail.com>
2020-04-18 11:47:01 +0200

    regular expression Validating Password

---
+++ regular-expressions-validating-password.js
@@ -0,0 +1,9 @@
+let str = 'Aa#1aaabcde'
+let pattern = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+
+let res = str.match(pattern);
+if (res) {
+    console.log("Valid password");
+} else {
+    console.log("Not a valid password");
+}
