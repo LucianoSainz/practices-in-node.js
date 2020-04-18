87924daad14bc6eade0b64683c4748749a434fb9
luciaoSainz <danielsainz28@gmail.com>
2020-04-18 11:41:41 +0200

    regular expression Find number of tags

---
+++ regular-expressionsTwo.js
@@ -0,0 +1,29 @@
+let fs = require('fs');
+let filename = 'data.txt';
+let str = fs.readFileSync(filename).toString();
+let pattern = /<(\"[^\"]*\"|[^'\">])*>/gim;
+
+let myarray = str.match(pattern);
+let len = myarray.length;
+console.log("Occurrences of pattern in the string is : " + len);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+")
